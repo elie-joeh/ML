c23433e88cf669e2199662df529b34fbc89290f3
elie-joeh <elie-joe.haikal@mail.mcgill.ca>
2020-06-03 11:41:28 -0400

    cnn modified

--- NLP/CNN.py
+++ NLP/CNN.py
@@ -1,28 +1,20 @@
 import tensorflow as tf
+from tensorflow import keras
 import numpy as np
 from scipy import sparse
-from keras.models import Sequential
-from keras import layers
+from tensorflow.keras.models import Sequential
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from keras.regularizers import l2
 import gensim as gm
 import re
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.utils import to_categorical
-from keras.layers import Embedding
-from keras.layers import Input
-from keras.layers import Conv1D
-from keras.layers import MaxPooling1D
-from keras.layers import Flatten
-from keras.layers import Dense
-from numpy import savetxt
+from tensorflow.keras import regularizers
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.layers import Embedding, Input, Conv1D, MaxPooling1D, Flatten, Dense, BatchNormalization, Dropout
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
 
-
-imdb_train_path = "..\Datasets\IMDB-train.txt"
-imdb_validation_path = "..\Datasets\IMDB-valid.txt"
-imdb_test_path = "..\Datasets\IMDB-test.txt"
 plt.style.use('ggplot')
 
 '''
@@ -37,79 +29,57 @@ and output dimensions as columns
 
 '''
 
+imdb_train_path = "./datasets/IMDB-train.txt"
+imdb_validation_path = "./datasets/IMDB-valid.txt"
+imdb_test_path = "./datasets/IMDB-test.txt"
+
 NUM_WORDS = 10000
 EMBEDDING_DIM = 300
 MAX_SEQUENCE_LENGTH = 1050
 
-def main():
-    imdb_train = fetchData(imdb_train_path)
-    imdb_val = fetchData(imdb_validation_path)
-    imdb_test = fetchData(imdb_test_path)
-
-    imdb_train = cleanData(imdb_train)
-    imdb_val = cleanData(imdb_val)
-    imdb_test = cleanData(imdb_test)
-
-    '''
-    imdb_train = imdb_train[:5000, :]
-    imdb_val = imdb_val[:2000, :]
-    imdb_test = imdb_test[:4000, :]
-    '''
-
-    X_train, y_train, X_val, y_val, X_test, y_test, word_index = prepareData(imdb_train, imdb_val, imdb_test)
-
-    embedding_matrix, vocabulary_size = generateEmbeddingMatrix(word_index)
-
-    storeEmbeddingMatrix(embedding_matrix)
-
-    '''
-    embedding_layer = Embedding(vocabulary_size,
-                                EMBEDDING_DIM,
-                                weights=[embedding_matrix],
-                                input_length=MAX_SEQUENCE_LENGTH,
-                                trainable=False)
-
-    model = createModel((embedding_layer))
-
-    model.summary()
-
-    history = model.fit(X_train, y_train, validation_data=(X_val, y_val),
-            epochs=20, batch_size=50,
-            shuffle=True,
-            verbose=False)
-
-    loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
-    print("Training Accuracy: {:.4f}".format(accuracy))
-
-    plot_history(history)
-    '''
-
-def storeEmbeddingMatrix(embeddingMatrix):
-    savetxt('..\Model\embedding_matrix.csv', embeddingMatrix, delimiter=',')
+embedding_matrix = np.loadtxt('./datasets/embedding_matrix.csv', delimiter=',')
 
 
-
-def createModel(embedding_layer):
+def createModel(init='glorot_uniform', kernel_size=2, pool_size=5, weight_decay=1e-3):
     model = Sequential()
-    #model.add(Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32'))
-    model.add(embedding_layer)
+    model.add(Embedding(vocabulary_size,
+                        EMBEDDING_DIM,
+                        weights=[embedding_matrix],
+                        input_length=MAX_SEQUENCE_LENGTH,
+                        trainable=False))
 
-    model.add(Conv1D(128, 2, activation='relu'))
-    model.add(MaxPooling1D(3))
+    model.add(Conv1D(128, kernel_size, activation='relu', input_shape=(MAX_SEQUENCE_LENGTH,),
+                     kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size))
+    model.add(Dropout(0.7))
 
-    model.add(Conv1D(128, 2, activation='relu', input_shape=(MAX_SEQUENCE_LENGTH,)))
-    model.add(MaxPooling1D(3))
-    model.add(Conv1D(128, 2, activation='relu'))
-    model.add(MaxPooling1D(3))
+    model.add(Conv1D(128, kernel_size, activation='relu', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size))
+    model.add(Dropout(0.7))
+
+    model.add(Conv1D(128, kernel_size, activation='relu', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size))
+    model.add(Dropout(0.7))
+
+    model.add(Conv1D(128, kernel_size, activation='relu', kernel_regularizer=regularizers.l2(weight_decay)))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size))
+    model.add(Dropout(0.7))
+
     model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
     model.add(Dense(2, activation='softmax'))
 
     model.compile(loss='categorical_crossentropy',
-            optimizer='adadelta',
-            metrics=['acc'])
+                  optimizer='adam',
+                  metrics=['acc'])
+
+    print(model.summary())
 
     return model
+
 
 def generateEmbeddingMatrix(word_index):
     word_vec = gm.models.KeyedVectors.load_word2vec_format('../Model/GoogleNews-vectors-negative300.bin', binary=True)
@@ -128,6 +98,7 @@ def generateEmbeddingMatrix(word_index):
     del word_vec
 
     return embedding_matrix, vocabulary_size
+
 
 def plot_history(history):
     print(history.history.items())
@@ -173,6 +144,7 @@ def prepareData(imdb_train, imdb_val, imdb_test):
 
     return X_train, y_train, X_val, y_val, X_test, y_test, word_index
 
+
 def cleanData(dataset):
     for row in dataset:
         row[0] = row[0].replace('<br />', ' ').lower()
@@ -180,6 +152,7 @@ def cleanData(dataset):
         row[0] = re.sub(r'\s+', ' ', row[0])
 
     return dataset
+
 
 def fetchData(path):
     dataset = []
@@ -193,8 +166,60 @@ def fetchData(path):
     np.random.shuffle(dataset)
     return np.array(dataset)
 
+
 def fetchSparseMatrix(path):
     return np.asarray(sparse.load_npz(path).todense())
 
-if __name__ == "__main__":
-    main()
+
+imdb_train = fetchData(imdb_train_path)
+imdb_val = fetchData(imdb_validation_path)
+imdb_test = fetchData(imdb_test_path)
+
+imdb_train = cleanData(imdb_train)
+imdb_val = cleanData(imdb_val)
+imdb_test = cleanData(imdb_test)
+
+X_train, y_train, X_val, y_val, X_test, y_test, word_index = prepareData(imdb_train, imdb_val, imdb_test)
+
+vocabulary_size = min(len(word_index) + 1, NUM_WORDS)
+
+# hyperparameters to tune
+weight_decay = [1e-4]
+kernel_size = [3]
+pool_size = [2]
+epochs = [20]
+batches = [100, 200, 500]
+init = ['glorot_uniform']
+
+'''
+model = createModel(vocabulary_size, kernel_size, strides, pool_size, activation, weight_decay)
+'''
+# create model
+model = KerasClassifier(build_fn=createModel, verbose=0)
+
+# do a grid search
+param_grid = dict(kernel_size=kernel_size, pool_size=pool_size, init=init, epochs=epochs, batch_size=batches,
+                  weight_decay=weight_decay)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=3)
+grid_result = grid.fit(X_train, y_train)
+
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+'''
+
+
+history = model.fit(X_train, y_train, validation_data=(X_val, y_val),
+        epochs=30, batch_size=50,
+        shuffle=True,
+        verbose=False)
+
+loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
+print("Training Accuracy: {:.4f}".format(accuracy))
+
+plot_history(history)
+'''
